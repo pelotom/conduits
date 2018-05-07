e1534a4260abb00104d2234f9a41f061442dcf60
Tom Crockett <pelotom@gmail.com>
2018-05-07 14:30:52 -0700

    WIP dataflow builder approach

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,19 +1,22 @@
-import { Conduit, connect, run } from '.';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/observable/of';
+import { Conduit, dataflow } from '.';
 
-it('simple run', async () => {
+it('basic', async () => {
   const c: Conduit<{ s: string }, { n: number }> = get => ({
     n: get('s').map(s => s.length),
   });
-  const n = await run(c, { s: 'hello' }).n.toPromise();
+  const d = dataflow().add(c);
+  const c2: Conduit<{}, { s: string }> = () => ({ s: Observable.of('hello') });
+  const d2 = d.add(c2);
+  const n = await d2.run().n.toPromise();
   expect(n).toBe(5);
 });
 
-it('loop', async () => {
-  const c: Conduit<{ n: number }, { n: number }> = get => ({
-    n: get('n').map(n => n + 1),
-  });
-
-  run(connect(c));
-});
+// it('loop', async () => {
+//   const c: Conduit<{ n: number }, { n: number }> = get => ({
+//     n: get('n').map(n => n + 1),
+//   });
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,53 @@
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 import 'rxjs/add/observable/of';
 
-export type Rec = Record<string, {} | null>;
+export type ConsistentWith<O> = Partial<O> & Record<string, {} | null>;
 
-export type ConsistentWith<O> = Partial<O> & Rec;
+export type GetInput<I> = <K extends keyof I>(name: K) => Observable<I[K]>;
+export type Outputs<O> = { [K in keyof O]: Observable<O[K]> };
 
-export type GetInput<I extends Rec> = <K extends keyof I>(name: K) => Observable<I[K]>;
-
-export interface Conduit<I extends Rec, O extends ConsistentWith<I>> {
-  (getInput: GetInput<I>): { [K in keyof O]: Observable<O[K]> };
+export interface Conduit<I, O> {
+  (getInput: GetInput<I>): Outputs<O>;
 }
 
-export type Omit<T, U> = {
-  [K in ({ [P in keyof T]: P } & { [P in keyof U]: never } & { [x: string]: never })[keyof T]]: T[K]
-};
-
-export function connect<I1 extends Rec, O1 extends ConsistentWith<I1>>(
-  c1: Conduit<I1, O1>,
-): Conduit<Omit<I1, O1>, Omit<O1, I1>>;
-export function connect<
-  I1 extends Rec,
-  O1 extends ConsistentWith<I1>,
-  I2 extends ConsistentWith<I1 & O1>,
-  O2 extends ConsistentWith<I1 & O1 & I2>
->(
-  c1: Conduit<I1, O1>,
-  c2: Conduit<I2, O2>,
-): Conduit<Omit<I1 & I2, O1 & O2>, Omit<O1 & O2, I1 & I2>>;
-export function connect<I extends Rec, O extends ConsistentWith<I>>(
-  ...conduits: Conduit<I, O>[]
-): Conduit<Omit<I, O>, Omit<O, I>> {
-  return outerGetInput => {
-    const inputs: { [K in keyof I]?: Subject<I[K]> } = {};
-
-    const getInput = <K extends keyof I>(name: K): Subject<I[K]> => {
-      if (!inputs[name]) inputs[name] = new Subject<I[typeof name]>();
-      return inputs[name]!;
-    };
-
-    conduits.forEach(conduit => {
-      const outputs = conduit(getInput);
-      for (const name in outputs) {
-        outputs[name].subscribe(getInput(name));
-      }
-    });
-  };
+export interface IncompleteDataflow<I, O> {
+  add<I2 extends ConsistentWith<I & O>, O2 extends ConsistentWith<I & O & I2>>(
+    other: Conduit<I2, O2>,
+  ): Dataflow<I & I2, O & O2>;
 }
 
-export function run<O extends Rec>(conduit: Conduit<{}, O>): { [K in keyof O]: Observable<O[K]> };
-export function run<I extends Rec, O extends ConsistentWith<I>>(
-  conduit: Conduit<I, O>,
-  sources: { [K in keyof I]: I[K] | Observable<I[K]> },
-): { [K in keyof O]: Observable<O[K]> };
-export function run<I extends Rec, O extends ConsistentWith<I>>(
-  conduit: Conduit<I, O>,
-  sources?: { [K in keyof I]: I[K] | Observable<I[K]> },
-): { [K in keyof O]: Observable<O[K]> } {
-  return conduit(k => {
-    const v = sources && sources[k];
-    return v instanceof Observable ? v : Observable.of(v);
-  });
+export interface CompleteDataflow<I, O extends I> extends IncompleteDataflow<I, O> {
+  run(): Outputs<O>;
+}
+
+export type Dataflow<I, O> = O extends I ? CompleteDataflow<I, O> : IncompleteDataflow<I, O>;
+
+export const dataflow = (): Dataflow<{}, {}> => createDataflow([]);
+
+function createDataflow<I, O>(conduits: Conduit<I, O>[]): Dataflow<I, O> {
+  return {
+    add: (other: Conduit<any, any>) => createDataflow([...conduits, other]),
+    run: (): Outputs<any> => {
+      const inputs: { [K in string]: Subject<any> } = {};
+
+      const getInput = (name: string): Subject<any> => {
+        if (!(name in inputs)) inputs[name] = new ReplaySubject();
+        return inputs[name];
+      };
+
+      const allOutputs: Outputs<any> = {};
+      conduits.forEach(conduit => {
+        const outputs = conduit(getInput);
+        for (const name in outputs) {
+          const output = outputs[name];
+          if (!(name in allOutputs)) allOutputs[name] = output;
+          output.subscribe(getInput(name));
+        }
+      });
+
+      return allOutputs;
+    },
+  } as any;
 }

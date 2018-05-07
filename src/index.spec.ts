486e25dea9f15f4231c6cb8bf651cd012b24d9a3
Tom Crockett <pelotom@gmail.com>
2018-05-07 01:39:28 -0500

    Dead end

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,6 @@
 import { Conduit, connect, run } from '.';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 
 it('simple run', async () => {
   const c: Conduit<{ s: string }, { n: number }> = get => ({
@@ -7,4 +8,12 @@ it('simple run', async () => {
   });
   const n = await run(c, { s: 'hello' }).n.toPromise();
   expect(n).toBe(5);
+});
+
+it('loop', async () => {
+  const c: Conduit<{ n: number }, { n: number }> = get => ({
+    n: get('n').map(n => n + 1),
+  });
+
+  run(connect(c));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/observable/of';
 
 export type Rec = Record<string, {} | null>;
 
 export type ConsistentWith<O> = Partial<O> & Rec;
 
+export type GetInput<I extends Rec> = <K extends keyof I>(name: K) => Observable<I[K]>;
+
 export interface Conduit<I extends Rec, O extends ConsistentWith<I>> {
-  (inputs: <K extends keyof I>(name: K) => Observable<I[K]>): { [K in keyof O]: Observable<O[K]> };
+  (getInput: GetInput<I>): { [K in keyof O]: Observable<O[K]> };
 }
 
 export type Omit<T, U> = {
@@ -28,7 +31,21 @@ export function connect<
 export function connect<I extends Rec, O extends ConsistentWith<I>>(
   ...conduits: Conduit<I, O>[]
 ): Conduit<Omit<I, O>, Omit<O, I>> {
-  return undefined as any;
+  return outerGetInput => {
+    const inputs: { [K in keyof I]?: Subject<I[K]> } = {};
+
+    const getInput = <K extends keyof I>(name: K): Subject<I[K]> => {
+      if (!inputs[name]) inputs[name] = new Subject<I[typeof name]>();
+      return inputs[name]!;
+    };
+
+    conduits.forEach(conduit => {
+      const outputs = conduit(getInput);
+      for (const name in outputs) {
+        outputs[name].subscribe(getInput(name));
+      }
+    });
+  };
 }
 
 export function run<O extends Rec>(conduit: Conduit<{}, O>): { [K in keyof O]: Observable<O[K]> };
@@ -40,8 +57,8 @@ export function run<I extends Rec, O extends ConsistentWith<I>>(
   conduit: Conduit<I, O>,
   sources?: { [K in keyof I]: I[K] | Observable<I[K]> },
 ): { [K in keyof O]: Observable<O[K]> } {
-  return conduit(p => {
-    const v = sources && sources[p];
+  return conduit(k => {
+    const v = sources && sources[k];
     return v instanceof Observable ? v : Observable.of(v);
   });
 }
